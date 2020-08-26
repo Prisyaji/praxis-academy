f7b21a3c52bcb79d5273f80e12683bf297fce1f5
Prisyaji <aji18suroso@gmail.com>
2020-08-26 17:06:19 +0700

    Belajar Lagi

---
+++ kemampuan-dasar/Belajar1/PersegiPanjang.java
@@ -0,0 +1,19 @@
+public class PersegiPanjang {
+    public static void main(String [] args) {
+        // deklarasi Variabel
+        int Panjang, Lebar, LuasPersegiPanjang;
+
+        // Deklarasi Nilai Variabel
+        Panjang = 20;
+        Lebar = 20;
+        
+        // Proses
+        LuasPersegiPanjang = Panjang * Lebar;
+
+        // Output
+        System.out.print ("Luas Persegi Panjang : ");
+        System.out.println(LuasPersegiPanjang);
+    
+    }
+    
+}
