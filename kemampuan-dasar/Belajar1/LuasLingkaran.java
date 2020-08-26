48a95a2fe67991f9e9ab58a831de72e050fca0ed
Prisyaji <aji18suroso@gmail.com>
2020-08-26 16:41:56 +0700

    Belajar Tipe data dan Variabel

---
+++ kemampuan-dasar/Belajar1/LuasLingkaran.java
@@ -0,0 +1,20 @@
+public class LuasLingkaran {
+    public static void main(String[] args) {
+        // Deklarasi Konstanta
+        final double PI = 3.14159;
+
+        // Deklarasi variabel jari2 dan luas
+        int jari2;
+        double luas;
+
+        // Memberi nilai awal ke variabel jari2 
+        jari2 = 7;
+
+        //Menghitung luas lingkaran
+        luas = PI * jari2 *jari2;
+
+        //output 
+        System.out.println( "jari-jari lingkaran = " + jari2 + "\nLuas Lingkaran = " + luas );
+        
+    }
+}
